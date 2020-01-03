3f38d9dbd2985bbc38da721e5ace33762fd3fb5f
LennyAtomz138 <lenny.atomz@gmail.com>
2020-01-03 14:08:37 -0500

    Adjusted Main.py by instantiating Textract and adding comments.

--- Source/Main.py
+++ Source/Main.py
@@ -3,6 +3,8 @@ Main program loop
 """
 
 import boto3
-import textract
+import textract  # This import may not be needed.
+
+client = boto3.client('textract')  # This may be all that's needed to get Textract called within this file.
 
 #  if __name__ == '__main__':
