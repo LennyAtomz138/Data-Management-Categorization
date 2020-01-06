4d4d8ae6213fa8f3569843d122997a37defe406e
tsbugbee <tsbugbee@gmail.com>
2020-01-06 13:43:20 -0500

    Basic implementation for the fileHandle class

--- Source/FileHandle.py
+++ Source/FileHandle.py
@@ -3,3 +3,16 @@ Class that handles all the individual files.
 -> Holds all of the information that a file would have.
 --> Filename, file type, potential tags, etc.
 """
+import string
+
+class fileHandle:
+    fileName = ""
+    fileType = ""
+    tags = []
+
+    def __init__(self, name, filetype):
+        self.fileName = name
+        self.fileType = filetype
+
+    def addtag(self, tag):
+        self.tags.append(tag)
