5c54ba7eda4b74326d1bb2cf4f0afd440412a982
LennyAtomz138 <lenny.atomz@gmail.com>
2020-01-07 06:17:27 -0500

    Worked on FileHandle.py.

--- Source/FolderHandle.py
+++ Source/FolderHandle.py
@@ -7,19 +7,24 @@ Tagged: Used to store files that WERE tagged during analysis (i.e.; Keyword(s) d
 
 
 class FolderHandle:
-    """A class that is used to manage the various types of folders."""
+    """A class that is used to manage the various types of folders.
+    Note that (currently) all lists will be shared by all instances of the FolderHandle class."""
 
     initial = []
     untagged = []
     tagged = []
 
-    def add_to_initial(self):
-        pass
+    def add_to_initial(self, file):
+        """Class method that adds a file to the Initial Folder."""
+        self.initial.append(file)
+        return
 
-    def add_to_untagged(self):
-        pass
+    def add_to_untagged(self, file):
+        """Class method that adds a file to the Untagged Folder."""
+        self.untagged.append(file)
+        return
 
-    def add_to_tagged(self):
-        pass
-
-    pass
+    def add_to_tagged(self, file):
+        """Class method that adds a file to the Tagged Folder."""
+        self.tagged.append(file)
+        return
