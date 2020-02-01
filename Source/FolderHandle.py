4b51d5ed48ae736385c943d9c480f16ac77cb57b
LennyAtomz138 <lenny.atomz@gmail.com>
2020-02-01 13:43:49 -0500

    Created lennys-branch.

--- Source/FolderHandle.py
+++ Source/FolderHandle.py
@@ -5,6 +5,12 @@ Untagged: Used to store files that were NOT tagged during analysis (i.e.; No key
 Tagged: Used to store files that WERE tagged during analysis (i.e.; Keyword(s) detected).
 """
 
+# TODO: Create method that creates a folder named after the keyword passed to it.
+# TODO: Modify the above method to create permutations of keyword folders when
+#  more than one tag is present for a given doc.
+# TODO: Create method that places the tagged documents into their respective folders.
+# TODO: Create method that sends the folders to respective S3 buckets in AWS upon completion.
+
 
 class FolderHandle:
     """A class that is used to manage the various types of folders.
