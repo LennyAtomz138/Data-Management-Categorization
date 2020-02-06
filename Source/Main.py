7238c001baf1056852e38012ef506c98ecda8b8c
LennyAtomz138 <lenny.atomz@gmail.com>
2020-02-05 21:35:38 -0500

    Added requirements.txt file

--- Source/Main.py
+++ Source/Main.py
@@ -2,15 +2,7 @@
 Main program loop
 """
 
-import boto3
-import textract  # This import may not be needed.
-
 from Source import CLIMenu
-
-
-# client = boto3.client('textract')  # This may be all that's needed to get Textract called within this file.
-
-#  if __name__ == '__main__':
 
 if __name__ == "__main__":
     CLIMenu.MainMenu()
